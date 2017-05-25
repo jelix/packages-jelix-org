b9507edc3ca861f53b22b70afab5e06d039e49de
Laurent Jouanneau <dev@ljouanneau.com>
2017-05-25 08:54:47 +0200

    Fix update.sh and urls

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-php satis/bin/satis build satis.json web/ $1
+if [ "$1" == "" ]; then
+    echo "repo name is missing (ex: foo/bar)"
+    exit 1
+fi
+
+php satis/bin/satis build --repository-url https://github.com/$1.git satis.json web/
